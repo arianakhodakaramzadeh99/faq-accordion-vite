7da460f4f6f937a4b4c0a4625f796dfaea094d5a
ariana <arianakhodakaramzadeh99@gmail.com>
2026-07-27 21:01:02 +0330

    done

--- src/main.js
+++ src/main.js
@@ -1,1 +1,16 @@
 import './style.css';
+const divOneEL = document.getElementById("divOne");
+const divTwoEL = document.getElementById("divTwo");
+const pTwoEL = document.getElementById("pTwo");
+console.log(pTwoEL);
+const divThreeEL = document.getElementById("threeOne");
+const divFourEL = document.getElementById("divFour");
+const divFiveEL = document.getElementById("divFour");
+const pOneEl = document.getElementById("pOne");
+divOneEL.addEventListener("click",()=>{
+divOneEL.classList.toggle("show");
+});
+divTwoEL.addEventListener("click",()=>{
+divTwoEL.classList.toggle("show1");
+
+})
